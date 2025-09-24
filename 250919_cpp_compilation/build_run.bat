3b2675e3021ac21432f5b7f110d43ab6e6bc3d9d
Ivanov Oleg <olivdm802@gmail.com>
2025-09-24 13:42:42 +0300

    Create bubble_sort

--- 250919_cpp_compilation/build_run.bat
+++ 250919_cpp_compilation/build_run.bat
@@ -2,8 +2,8 @@
 chcp 1251 > log
 del log 
 
-set MAIN=main.cpp
-set EXE=example.exe
+set MAIN="main.cpp bubble_sort.cpp"
+set EXE=bubble_sort.exe
 set CHARSET="-finput-charset=utf-8 -fexec-charset=windows-1251"
 
 if exist %EXE% del %EXE%
