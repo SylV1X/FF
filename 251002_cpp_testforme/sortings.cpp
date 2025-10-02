37e404d0a1a316a7fce6074918a8dddcf9334a15
Ivanov Oleg <olivdm802@gmail.com>
2025-10-02 20:47:59 +0300

    Create cpp_sort app

--- 251002_cpp_testforme/sortings.cpp
+++ 251002_cpp_testforme/sortings.cpp
@@ -1,7 +1,5 @@
 #include "sortings.hpp"
 
-using namespace std;
-
 void biv::sortings::bubble_sort(int* const arr, const int size) {
 	    for (int i = 0; i < size-1; i++) {     
         for (int j = 0; j < size-i-1; j++) {  
