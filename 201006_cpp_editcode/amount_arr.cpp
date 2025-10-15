783494f317497035efaec2bab3cbe71f40ebc5ee
SylV1X <olivdm802@gmail.com>
2025-10-15 14:57:48 +0300

    Delete 201006_cpp_editcode directory

--- 201006_cpp_editcode/amount_arr.cpp
+++
@@ -1,9 +0,0 @@
-#include "count_sorting_tools.hpp"
-
-void iod::amount(const int* arr, int* amount_arr, const int size){
-	
-	for (int i = 0; i < size; i++){
-		amount_arr[arr[i]]++;
-	}
-	
-}
--- 201006_cpp_editcode/io.hpp
+++
@@ -1,8 +0,0 @@
-#ifndef IO_HPP
-#define IO_HPP
-
-namespace iod {
-	void print(const char* const comment, int* arr, const int size);
-}
-
-#endif
