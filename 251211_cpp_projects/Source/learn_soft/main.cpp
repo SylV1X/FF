f260575409a57439d4a04bb58400f3084b56fb09
Ivanov Oleg <olivdm802@gmail.com>
2025-12-12 00:34:53 +0300

    Big Project Second Commit

---
+++ 251211_cpp_projects/Source/learn_soft/main.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+#include "menu.hpp"
+#include "menu_functions.hpp"
+#include "menu_items.hpp"
+
+int main()
+{	
+	const ssY::MenuItem* current = &ssY::MAIN;
+	do
+	{
+		current = current->func(current);
+	}
+	while (true);
+	
+	return 0;
+}
